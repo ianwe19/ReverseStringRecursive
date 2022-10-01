9d71045605acb37f657d4d8713ab9b1cdfb85b19
shibiwhy <ian.welch19@icloud.com>
2022-10-01 15:46:31 -0400

    Add project files.

---
+++ ReverseStringRecursive/ReverseStringRecursive.cpp
@@ -0,0 +1,38 @@
+// INCLUES
+#include <iostream>
+
+
+// CONSTANTS
+const int STRING_SIZE = 100;
+
+
+// PROTOTYPES
+int ReverseStringRecursive(char*);
+
+
+// MAIN
+int main() {
+	// create a string
+	char someString[STRING_SIZE] = "abcdefghijklmnopqrstuvwxyz";
+
+	// display the string before being reversed
+	std::cout << "The string contains: \n";
+	std::cout << someString << "\n\n";
+
+	// make the call to the recursive function
+	std::cout << "CALL THE REVERSING FUNCTION\n\n";
+	ReverseStringRecursive(someString);
+
+	// display the string after being reversed
+	std::cout << "The string contains: \n";
+	std::cout << someString << "\n";
+
+	// exit program
+	return 0;
+}
+
+
+// FUNCTION IMPLEMENTATIONS
+int ReverseStringRecursive(char* reverseMe) {
+	// YOUR IMPLEMENTATION GOES HERE...
+}
